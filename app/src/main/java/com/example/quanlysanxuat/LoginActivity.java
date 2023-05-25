80ed2fa9cb2f771cde3c29be7a9c17d6ea8c4ed9
MrA <nva12082001@gmail.com>
2023-05-26 01:12:07 +0700

    26/5

--- app/src/main/java/com/example/quanlysanxuat/LoginActivity.java
+++ app/src/main/java/com/example/quanlysanxuat/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +19,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         TextView username = findViewById(R.id.username);
         TextView password = findViewById(R.id.password);
 
